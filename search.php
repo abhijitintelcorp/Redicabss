01b7435caf8823fdb765469b42d6b9cc00a242f4
sureshnirmala <nirmalakalluru1980@gmail.com>
2022-01-28 01:31:35 -0600

    some changes in search page

--- search.php
+++ search.php
@@ -41,6 +41,8 @@ error_reporting(0);
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 </head>
 <body>
+ <!--Body-->
+<h3>Search page</h3>
 
 <!-- Start Switcher -->
 <?php include('includes/colorswitcher.php');?>
