c8416acc8c290e18472e872f69d48c2ba82a1d68
rednuht93 <alok.intelcoro21@gmail.com>
2022-01-29 11:46:17 +0530

    updated

--- admin/new-bookings.php
+++ admin/new-bookings.php
@@ -62,7 +62,6 @@ else{
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-md-12">
-=======
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
