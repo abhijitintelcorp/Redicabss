22f955e1253abb3993c247b577f24ed8a7178981
rednuht93 <alok.intelcoro21@gmail.com>
2022-02-03 10:00:59 +0530

    Updated

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-2 col-md-2">
-          <div class="logo"> <a href="index.php"><img src="./assets/images/redicabs_logo.png" height="180" width="250" alt="image"/></a> </div>
+          <div class="logo"> <a href="index.php"><img src="./assets/images/redicabs_logo.png" height="200" width="250" alt="image"/></a> </div>
         </div>
         <div class="col-sm-10 col-md-10" >
           <div class="header_info" style="padding: 50px;">
